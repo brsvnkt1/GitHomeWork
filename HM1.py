b83386d437409fe12824c2a911f30b39b73fbdfa
brsvnkt1 <brsvnkt1@ya.ru>
2020-08-24 16:58:27 +0300

    Дз 1

---
+++ HM1.py
@@ -0,0 +1,12 @@
+a = 5
+b = 10
+print(a)
+print(b)
+c = input("Введите число ")
+d = input("Введите число ")
+e = input("Введите фразу ")
+f = input("Введите фразу ")
+print(c)
+print(d)
+print(e)
+print(f)

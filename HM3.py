e1c3147125e071799559c4083a2337dc7f25d15c
brsvnkt1 <brsvnkt1@ya.ru>
2020-08-24 17:07:41 +0300

    Delete HM3.py

--- HM3.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Введите число > "))
-print("Считаем сумму n+nn+nnn")
-preob = str(n)
-templ1 = preob + preob
-templ2 = preob + preob + preob
-nn = int(templ1)
-nnn = int(templ2)
-summa = n + nn + nnn
-print(summa)

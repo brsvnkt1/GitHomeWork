b18bbf459fb3edc78c685c9af5fbd0aaffd5f72d
brsvnkt1 <brsvnkt1@ya.ru>
2020-09-01 11:28:44 +0300

    Добавлена домашка по третьему уроку

---
+++ HM3.1.py
@@ -0,0 +1,22 @@
+def div(*args):
+
+    try:
+        arg1 = int(input("Введите девидент "))
+        arg2 = int(input("введите делитель "))
+        res = arg1 / arg2
+    except ValueError:
+        return 'Value error'
+    except ZeroDivisionError:
+        return "Wrong devider! You can't use zero as a devider"
+
+    return res
+
+    '''
+    if arg2 != 0:
+        return arg1 / arg2
+    else:
+        print("Wrong number! Devider can't be null")
+    '''
+
+
+print(f'result  {div()}')
